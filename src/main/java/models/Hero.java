1af1298dac19832470d855bb1ba3fafd725c8ed5
Chillsthrills <pwan.wa100@gmail.com>
2020-01-07 19:51:11 +0300

    Hero.java edit

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,53 @@
 package models;
+import java.util.ArrayList;
 
 public class Hero {
+    private String Heroname;
+    private int Heroage;                //class properties
+    private String Heropower;
+    private String Heroweakness;
+    private int Heroid;
+    private static ArrayList<Hero> allHeroes = new ArrayList<>();
+
+    public Hero(String name,String power,String weakness,int age, int id){
+        this.Heroname = name;
+        this.Heroid = id;
+        this.Heroage = age;             //constructor to create new class objects
+        this.Heropower = power;
+        this.Heroweakness = weakness;
+        allHeroes.add(this);
+    }
+
+    public String getName(){
+        return Heroname;
+    }
+
+    public int age(){
+        return Heroage;
+    }
+
+    public String getPower(){
+        return Heropower;
+    }          // getters and setters to access private variables
+
+    public String getWeakness(){
+        return Heroweakness;
+    }
+
+    public int getId() {
+        return Heroid;
+    }
+
+    public void setId(int id) {
+        this.Heroid = id;
+    }           //id Setter
+
+    public static ArrayList<Hero> getAllHeroes() {
+        return allHeroes;
+    }
+
+
 }
+
+
+
